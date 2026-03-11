fde93ab3bb26cf83e548e29a8c4b627c253451bc
abuarfan <keluargaabuarfan@gmail.com>
2026-03-11 11:27:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 🔥 NAIKKAN ANGKA VERSI INI SETIAP KALI UPDATE KODE KE VERCEL 🔥
-const CACHE_NAME = 'ljk-scanner-v6.0'; 
+const CACHE_NAME = 'ljk-scanner-v5.9'; 
 
 const urlsToCache = [
     './',
@@ -58,3 +58,4 @@ self.addEventListener('fetch', event => {
         })
     );
 });
+
